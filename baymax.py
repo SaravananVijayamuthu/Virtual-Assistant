44435d1165604325a894d3f42094dfbb215a1dae
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-23 11:23:11 +0530

    created speak func

--- baymax.py
+++ baymax.py
@@ -1,5 +1,13 @@
 import pyttsx3  # text to speech -> https://pypi.org/project/pyttsx3/
 
-engine = pyttsx3.init() #initial func. obj creation
-engine.say("Hi, Baymax here")
-engine.runAndWait()
+engine = pyttsx3.init()  # initial func. obj creation
+
+
+############
+# Speech
+############
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+speak("Hi, Baymax here how you doing?")
