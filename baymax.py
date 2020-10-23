12e7c2ca03f52ba7a05daeda3411f36d41837215
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-23 11:45:11 +0530

    created DateTime func

--- baymax.py
+++ baymax.py
@@ -1,4 +1,5 @@
 import pyttsx3  # text to speech -> https://pypi.org/project/pyttsx3/
+import datetime
 
 engine = pyttsx3.init()  # initial func. obj creation
 
@@ -10,4 +11,31 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("Hi, Baymax here how you doing?")
+
+# speak("Hi, Baymax here how you doing?")
+
+
+############
+# Time
+############
+def time():
+    Time = datetime.datetime.now().strftime("%H:%M:%S")  # converting to str format
+    speak(Time)
+
+
+# time()
+
+
+############
+# Date
+############
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak(date)
+    speak(month)
+    speak(year)
+
+
+# date()
