d6ca36366e0babc0c6b53a66dba99daba842863c
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-23 11:58:34 +0530

    greet on time basis

--- baymax.py
+++ baymax.py
@@ -50,6 +50,15 @@ def greet():
     time()
     speak("The current date is")
     date()
+    hour = datetime.datetime.now().hour
+    if hour>=6 and hour<12:
+        speak("Hey,Good Morning. Have a nice day!")
+    elif hour>=12 and hour<18:
+        speak("Hey, Good Afternoon. Have a Healthy meal!")
+    elif hour>=18 and hour<24:
+        speak("Hey, Good Evening. Don't forget to go for a walk")
+    else:
+        speak("Good Night, Sweet dreams") 
     speak("Hi I'm Baymax your virtual partner, How can I help you?")
 
 
